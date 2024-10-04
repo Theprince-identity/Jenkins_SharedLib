ccb66d9027173f19479aadf410d2866c5cbe4bf1
Prince <Theprinceidentity@gmail.com>
2024-10-04 16:40:11 +0530

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,14 @@
-def call(){
-  timeout(time: 5, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: false
-  }
+def call() {
+    // Define SonarQube Scanner environment and tool
+    def scannerHome = tool 'Sonar' // Replace 'SonarQubeScanner' with the correct tool name configured in Jenkins
+
+    withSonarQubeEnv('Sonar') { // 'SonarQube' refers to the name of your SonarQube server configuration in Jenkins
+        // Run SonarQube analysis with SCM disabled
+        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=wanderlust -Dsonar.scm.disabled=true"
+
+        // Wait for Quality Gate with timeout
+        timeout(time: 5, unit: "MINUTES") {
+            waitForQualityGate abortPipeline: false
+        }
+    }
 }
